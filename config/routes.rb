5f99a12fe16694e2e4b190571a3ceea9add5205d
Etinosa <em.obanor@gmail.com>
2015-06-25 20:40:22 +0100

    Created tasks scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  resources :tasks
   devise_for :views
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
